b83fc13fb6865c7c5bb332449295c2c69919d308
TJisstillhere <timothyjohnasada25@gmail.com>
2026-04-14 20:28:06 +0800

    second push

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // We will put things like creating posts, answering tracer studies, 
     // and sending messages inside here later!
 });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
